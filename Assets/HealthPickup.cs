5f77d2d02d7578bd0444c9058beb085204759404
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2025-11-17 22:27:28 -0500

    Health pickup
    - added health pickups, they increase players health by 10

---
+++ Assets/HealthPickup.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public string playerTag = "Player";
+    public float healAmount = 10f;
+
+    public BikeHealth bikeHealth;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if (bikeHealth != null)
+            {
+                // Check if adding heal would go over max
+                float newHealth = bikeHealth.Health + healAmount;
+
+                // If it's above max health, force it to max
+                if (newHealth > bikeHealth.maxHealth)
+                {
+                    
+                    float amountNeeded = bikeHealth.maxHealth - bikeHealth.Health;
+                    bikeHealth.SetHealth(amountNeeded);
+                }
+                else
+                {
+                    
+                    bikeHealth.SetHealth(healAmount);
+                }
+            }
+
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
+
