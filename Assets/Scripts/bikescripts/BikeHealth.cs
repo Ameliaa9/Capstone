f7ff37ac8e7f0728f4557d9db95b976ec2d4f7ff
hajihajhaj <163481952+hajihajhaj@users.noreply.github.com>
2025-11-26 00:44:31 -0500

    added respawn and more
    -respawn with full health when player hits 0
    -replaced coins with coffee cups
    -added animation to upgrade shop and depot on mega map

--- Assets/Scripts/bikescripts/BikeHealth.cs
+++ Assets/Scripts/bikescripts/BikeHealth.cs
@@ -10,6 +10,8 @@ public class BikeHealth : MonoBehaviour
     private RectTransform healthBar;
     [SerializeField]
     private TextMeshProUGUI value;
+    [SerializeField] private Transform respawnPoint;   
+
 
     public float Health;
     public float maxHealth;
@@ -36,5 +38,34 @@ public class BikeHealth : MonoBehaviour
         float newWidth = (Health / maxHealth) * width;
         healthBar.sizeDelta = new Vector2(newWidth, height);
         value.text = Health.ToString();
+
+        if (Health <= 0)
+        {
+            Respawn();
+        }
     }
+
+    private void Respawn()
+    {
+        // teleport bike
+        if (player != null && respawnPoint != null)
+        {
+            var rb = player.GetRigidbody();
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+
+            player.transform.position = respawnPoint.position;
+            player.transform.rotation = respawnPoint.rotation;
+        }
+
+        // reset health back to full
+        Health = maxHealth;          
+        float newWidth = (Health / maxHealth) * width;
+        healthBar.sizeDelta = new Vector2(newWidth, height);
+        value.text = Health.ToString();
+    }
+
 }
