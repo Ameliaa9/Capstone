9fa80aaf4c7d13e46c0dd8e3b1793e82ee9d3422
SHIBINU001 <000928128@mohawkcollege.ca>
2026-02-03 10:13:33 -0500

    add voice test.cs will test at class

---
+++ Assets/Scripts/Audioscripts/VoiceTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class VoiceTest : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5)) AudioManager.I.PlayRatingVoice(5);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) AudioManager.I.PlayRatingVoice(4);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) AudioManager.I.PlayRatingVoice(3);
+    }
+}
+
