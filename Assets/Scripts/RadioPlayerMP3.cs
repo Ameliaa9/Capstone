a3ea5b0c9168ef8fe172ed0092c56081b1f7ba14
BuddMcCormack <cmccormack1437@gmail.com>
2026-02-22 10:24:39 -0500

    Radio System Pt.2
    - Songs now play
    - Random song plays on start
    - Next song and Previous song buttons now working
    - Working shuffle button

--- Assets/Scripts/RadioPlayerMP3.cs
+++ Assets/Scripts/RadioPlayerMP3.cs
@@ -1,3 +1,4 @@
+using KikiNgao.SimpleBikeControl;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,42 +8,100 @@ public class RadioPlayerMP3 : MonoBehaviour
     [Header("List of Songs")]
     [SerializeField]
     private Music[] musicList;
+
     private int songIndex;
-    private AudioSource songAudioSource;
-    private Sprite songAlbumCover;
+    private int songPreviousIndex;
     private float songDuration;
+
+    [SerializeField]
+    private Image songAlbumCoverImage;
 
     [SerializeField]
     private TextMeshProUGUI phoneTextUI;
 
     private bool isShuffling = false;
-    
+
+    [SerializeField]
+    private TextMeshProUGUI shufflingStatus;
+
+    private AudioSource songAudioSource;
+
 
     void Start()
     {
         songIndex = Random.Range(0, musicList.Length);
         songAudioSource = GetComponent<AudioSource>();
-        songAlbumCover = musicList[songIndex].albumCover;
-        songDuration = musicList[songIndex].musicDuration;
-        phoneTextUI.text = musicList[songIndex].name;
-
-        songAudioSource.resource = musicList[songIndex].musicAudioClip;
-        songAudioSource.Play();
+        PlaySong();
     }
 
     void Update()
     {
-        
+        GameManager.UnlockCursor();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlaySong()
+    {
+        songPreviousIndex = songIndex;
+        songAlbumCoverImage.sprite = musicList[songIndex].albumCover;
+        songDuration = musicList[songIndex].musicDuration;
+        phoneTextUI.text = musicList[songIndex].name;
+
+        songAudioSource.clip = musicList[songIndex].musicAudioClip;
+        songAudioSource.Play();
     }
 
     public void NextSong()
     {
+        songAudioSource.Stop();
 
+        if (isShuffling)
+        {
+            songIndex = Random.Range(0, musicList.Length);
+            if (songIndex != songPreviousIndex)
+            {
+                PlaySong();
+                Debug.Log("CASE 0 : Shuffle On, New song picked");
+            }
+            else
+            {
+                NextSong();
+                Debug.Log("CASE 1 : Shuffle On, Next song re-called");
+            }
+        }
+        else
+        {
+            if (songIndex < musicList.Length - 1)
+            {
+                songIndex++;
+                PlaySong();
+                Debug.Log("CASE 2 : Shuffle Off, Song index increased");
+            }
+            else
+            {
+                songIndex = 0;
+                PlaySong();
+                Debug.Log("CASE 3 : Shuffle Off, Song index reset");
+            }
+        }
     }
 
     public void PreviousSong()
     {
-
+        songAudioSource.Stop();
+        if (songIndex > 0)
+        {
+            songIndex--;
+            PlaySong();
+            Debug.Log("CASE 4 : Previous Song, Song index decreased");
+        }
+        else
+        {
+            songIndex = musicList.Length - 1;
+            PlaySong();
+            Debug.Log("CASE 5 : Previous Song, Song index maxed");
+        }
     }
 
     public void ToggleShuffle()
@@ -50,10 +109,12 @@ public class RadioPlayerMP3 : MonoBehaviour
         if (isShuffling)
         {
             isShuffling = false;
+            shufflingStatus.text = "Shuffle is OFF";
         }
         else
         {
             isShuffling = true;
+            shufflingStatus.text = "Shuffle is ON";
         }
     }
 }
