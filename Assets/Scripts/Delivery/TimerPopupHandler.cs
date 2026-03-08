b9e98fd35610952e7571412a526ca2442cd81d24
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2026-03-07 19:04:23 -0500

    Messages animation stuff
    - Removed code I don't need anymore
    - editing the keyframes for the messages

--- Assets/Scripts/Delivery/TimerPopupHandler.cs
+++ Assets/Scripts/Delivery/TimerPopupHandler.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
-using System.Collections;
 
 public class TimerPopupHandler : MonoBehaviour
 {
     public GameObject popupImage;
-    public Animator popupAnimator;
 
     public void ShowPopup()
     {
         if (popupImage != null)
             popupImage.SetActive(true);
-
-        if (popupAnimator != null)
-            popupAnimator.Play("MessageSlideCombined", 0, 0f); 
     }
 }
