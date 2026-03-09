77045ac10a7c69ea67a5d2a310d7ebbcade265c9
hajihajhaj <163481952+hajihajhaj@users.noreply.github.com>
2026-03-08 20:02:52 -0400

    pickups update
    -made them respawn after 30 seconds
    -made the coffee a speed boost

--- Assets/Scripts/pickups/CoinDisappear.cs
+++ Assets/Scripts/pickups/CoinDisappear.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class DisappearOnTrigger : MonoBehaviour
 {
-    public string playerTag = "Player";
+    public string playerTag = "Bike";
 
     public TaskManager coinTaskManager;
+
+    public float boostAmount = 12f;
+    public float boostDuration = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +16,12 @@ public class DisappearOnTrigger : MonoBehaviour
         if (coinTaskManager != null)
         {
             coinTaskManager.OnCoinCollected();
+        }
+
+        BikeMovement bike = other.GetComponent<BikeMovement>();
+        if (bike != null)
+        {
+            bike.ApplySpeedBoost(boostAmount, boostDuration);
         }
 
         gameObject.SetActive(false);
