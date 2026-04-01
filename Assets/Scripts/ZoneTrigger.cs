f0c4e5b2d275a093d61dba5951d08578318dd470
SHIBINU001 <000928128@mohawkcollege.ca>
2026-04-01 01:05:56 -0400

    Zone 1 for decrease loading time (24.56 seconds)

--- Assets/Scripts/ZoneTrigger.cs
+++ Assets/Scripts/ZoneTrigger.cs
@@ -1,14 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class ZoneTrigger : MonoBehaviour
 {
     [SerializeField] private string zoneName = "Zone 1";
     [SerializeField] private ZoneNPCController[] zoneNPCs;
+    [SerializeField] private float initialDelay = 3f;
+
+    private bool playerInside = false;
+
+    private void Start()
+    {
+        StartCoroutine(InitialZoneCheck());
+    }
+
+    private IEnumerator InitialZoneCheck()
+    {
+        yield return new WaitForSeconds(initialDelay);
+
+        if (!playerInside)
+        {
+            foreach (ZoneNPCController npc in zoneNPCs)
+            {
+                if (npc != null)
+                    npc.SetActiveState(false);
+            }
+
+            Debug.Log(zoneName + " NPCs set to sleep after initial delay.");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bike"))
         {
+            playerInside = true;
             Debug.Log("Entered " + zoneName);
 
             foreach (ZoneNPCController npc in zoneNPCs)
@@ -23,6 +49,7 @@ public class ZoneTrigger : MonoBehaviour
     {
         if (other.CompareTag("Bike"))
         {
+            playerInside = false;
             Debug.Log("Exited " + zoneName);
 
             foreach (ZoneNPCController npc in zoneNPCs)
